6a8eec3b6ea7583da0a5d4677dfac7bd6aac30c5
Rory Bradford <roryrjb@gmail.com>
2021-11-20 22:27:45 +0000

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,45 @@ extern crate chrono;
 extern crate rexif;
 
 use chrono::NaiveDateTime;
+use chrono::ParseError;
+use rexif::ExifError;
 use rexif::ExifTag::DateTime as ExifDateTime;
 use std::env;
 use std::fs;
 use std::path::Path;
-use std::process::exit;
 
-fn main() {
+#[derive(Debug)]
+enum Error {
+    Exif(ExifError),
+    Parse(ParseError),
+    Io(std::io::Error)
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let filename = args.get(1).unwrap();
 
-    match rexif::parse_file(&filename) {
-        Ok(exif) => {
-            for entry in &exif.entries {
-                if entry.tag == ExifDateTime {
-                    let dt = NaiveDateTime::parse_from_str(
-                        &entry.value_more_readable,
-                        "%Y:%m:%d %H:%M:%S",
-                    )
-                    .unwrap();
+    let exif = rexif::parse_file(&filename).map_err(Error::Exif)?;
 
-                    let new_name = format!("./{}", dt.format("%Y/%m_%B/%Y_%m_%d_%H_%M_%S.jpg"));
-                    let dirname = Path::new(&new_name).parent().unwrap();
-                    fs::create_dir_all(dirname).unwrap();
-                    fs::rename(filename, &new_name).unwrap();
-                    println!("moved file to {}", &new_name);
+    for entry in &exif.entries {
+        if entry.tag == ExifDateTime {
+            let dt = NaiveDateTime::parse_from_str(
+                &entry.value_more_readable,
+                "%Y:%m:%d %H:%M:%S",
+            ).map_err(Error::Parse)?;
 
-                    break;
-                }
-            }
+            let new_name = format!("./{}", dt.format("%Y/%m_%B/%Y_%m_%d_%H_%M_%S.jpg"));
+            let dirname = Path::new(&new_name).parent().unwrap();
+            fs::create_dir_all(dirname).map_err(Error::Io)?;
+            fs::rename(filename, &new_name).map_err(Error::Io)?;
+            eprintln!("moved file to {}", &new_name);
+
+            break;
         }
-        Err(e) => {
-            println!("Error: {}.", &e);
-            exit(1);
-        }
+    
     }
+
+    Ok(())
 }

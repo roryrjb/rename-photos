b330e92c6adbdc42c7e1b41815208e0a8da13edc
Rory Bradford <roryrjb@gmail.com>
2021-11-21 11:17:35 +0000

    Add multiple file support
    Assume we are only interested in *.jpgs in the current working directory only

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,42 @@ extern crate chrono;
 extern crate rexif;
 
 use chrono::NaiveDateTime;
-use chrono::ParseError;
-use rexif::ExifError;
+use glob::glob;
 use rexif::ExifTag::DateTime as ExifDateTime;
-use std::env;
 use std::fs;
 use std::path::Path;
 
-#[derive(Debug)]
-enum Error {
-    Exif(ExifError),
-    Parse(ParseError),
-    Io(std::io::Error)
-}
-
-type Result<T> = std::result::Result<T, Error>;
-
-fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let filename = args.get(1).unwrap();
-
-    let exif = rexif::parse_file(&filename).map_err(Error::Exif)?;
+fn get_date_from_photo(filename: &str) -> Option<NaiveDateTime> {
+    let exif = rexif::parse_file(&filename).ok()?;
 
     for entry in &exif.entries {
         if entry.tag == ExifDateTime {
-            let dt = NaiveDateTime::parse_from_str(
-                &entry.value_more_readable,
-                "%Y:%m:%d %H:%M:%S",
-            ).map_err(Error::Parse)?;
-
-            let new_name = format!("./{}", dt.format("%Y/%m_%B/%Y_%m_%d_%H_%M_%S.jpg"));
-            let dirname = Path::new(&new_name).parent().unwrap();
-            fs::create_dir_all(dirname).map_err(Error::Io)?;
-            fs::rename(filename, &new_name).map_err(Error::Io)?;
-            eprintln!("moved file to {}", &new_name);
-
-            break;
+            return NaiveDateTime::parse_from_str(&entry.value_more_readable, "%Y:%m:%d %H:%M:%S")
+                .ok();
         }
-    
+    }
+
+    None
+}
+
+fn main() -> Result<(), std::io::Error> {
+    for entry in glob("*.jpg").unwrap() {
+        match entry.unwrap().to_str() {
+            Some(f) => {
+                let date = match get_date_from_photo(f) {
+                    Some(date) => date,
+                    None => continue,
+                };
+
+                let new_name = format!("./{}", date.format("%Y/%m_%B/%Y_%m_%d_%H_%M_%S.jpg"));
+                let dirname = Path::new(&new_name).parent().unwrap();
+                fs::create_dir_all(dirname)?;
+                fs::rename(f, &new_name)?;
+                eprintln!("moved file to {}", &new_name);
+            }
+
+            None => continue,
+        }
     }
 
     Ok(())
